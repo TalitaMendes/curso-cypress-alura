b3359a626ac9053263fe40734aadb3867a5a6217
TalitaMendes <talita-mendes-silva@outlook.com>
2024-01-20 01:10:20 -0300

    Create api-alurapic.cy.js

---
+++ cypress/e2e/api-alurapic.cy.js
@@ -0,0 +1,28 @@
+describe('API Alurapic', () => {
+    //it('Dados da API', () => {
+      //cy.request({
+        //method: "POST",
+        //url: 'http://localhost:3000/user/login',
+        //body: Cypress.env()
+     //}).then((res) => {
+        //expect(res.status).to.be.equal(200)
+        //expect(res.body).is.not.empty
+        //expect(res.body).to.have.property('id')
+        //expect(res.body.id).to.be.equal(4)
+      //})
+    //})
+   it('Fotos dos usuarios', () => {
+      //const tempoEsperado = Math.random() * 50;
+
+      cy.request({
+         method: 'GET',
+         url: 'http://localhost:3000/talita123/photos',
+      }).then((res) => {
+         expect(res.status).to.be.equal(200)
+         expect(res.body).is.not.empty
+         expect(res.body[0]).to.have.property('description')
+         expect(res.body[0].description).to.be.equal('Hamster')
+         //expect(res.duration).to.be.lte(tempoEsperado)
+       })
+    })
+  })
